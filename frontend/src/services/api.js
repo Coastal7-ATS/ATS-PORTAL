70699f2214bd60c4501e75d1de9a20093836bc32
Jayanth Kotte <136954509+jayanthkotte@users.noreply.github.com>
2025-08-01 15:01:17 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://recruitment-backend:8000',
   headers: {
     'Content-Type': 'application/json',
   },
